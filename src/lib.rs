74d01a5dc27c0db15610c98084f9ab2caaf3f7ad
Rocky_Felix <chris.davidoff>
2023-02-03 10:28:48 -0700

    p45 rock and stone

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,22 @@
+use std::net::TcpListener;
+
 use actix_web::{HttpServer, App, web, Responder, HttpResponse, dev::Server};
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
-pub fn run() -> std::io::Result<Server> {
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+pub fn run(listener: TcpListener) -> std::io::Result<Server> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
-    .bind("0.0.0.0:8000")?
+    .listen(listener)?
     .run();
 
     Ok(server)
